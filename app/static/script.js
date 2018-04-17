2d356b3d47276e467d344b1de618e413f08818eb
Anirudh Maddula <aam252@cornell.edu>
2018-04-17 15:37:06 -0400

    adding slider bar and stuff

--- app/static/script.js
+++ app/static/script.js
@@ -516,7 +516,7 @@ function autocomplete(inp, arr) {
       a.setAttribute("id", this.id + "autocomplete-list");
       a.setAttribute("class", "autocomplete-items");
 
-      
+
       /*append the DIV element as a child of the autocomplete container:*/
       this.parentNode.appendChild(a);
       /*for each item in the array...*/
